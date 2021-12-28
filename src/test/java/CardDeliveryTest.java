d61aaafc3e8d7fd94ff31d79bcc6c14b855208c7
aa_maslov <smart.maslov@gmail.com>
2021-12-28 05:37:13 +0300

    Внесены исправления

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -6,29 +6,39 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 
 public class CardDeliveryTest {
 
+    String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+    String planningDate = generateDate(5);
+
     @Test
     public void shouldCheckingTheCorrectForm() {
         open("http://localhost:9999/");
         $("[data-test-id='city'] input").setValue("Москва");
-        $("[data-test-id='date'] input").setValue("29.12.2021");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(planningDate);
         $("[data-test-id='name'] input").setValue("Аркадий Паровозов");
         $("[data-test-id='phone'] input").setValue("+79112650042");
         $("[data-test-id='agreement']").click();
         $("[class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
         $("[data-test-id='notification']").shouldBe(Condition.appear, Duration.ofSeconds(15));
+        $(".notification__content").shouldBe(visible).
+                shouldHave(exactText("Встреча успешно забронирована на " + planningDate));
 
     }
     @Test
     public void shouldCheckingTheFormNoPhone() {
         open("http://localhost:9999/");
-        $("[data-test-id='city'] input").setValue("Москва");
-        $("[data-test-id='date'] input").setValue("29.12.2021");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(planningDate);
         $("[data-test-id='name'] input").setValue("Аркадий Паровозов");
         $("[data-test-id='agreement']").click();
         $("[class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
@@ -38,7 +48,8 @@ public class CardDeliveryTest {
     @Test
     public void shouldCheckingTheFormNoCity() {
         open("http://localhost:9999/");
-        $("[data-test-id='date'] input").setValue("29.12.2021");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(planningDate);
         $("[data-test-id='name'] input").setValue("Аркадий Паровозов");
         $("[data-test-id='phone'] input").setValue("+79112650042");
         $("[data-test-id='agreement']").click();
@@ -50,13 +61,56 @@ public class CardDeliveryTest {
     public void shouldCheckingTheFormNoCheckbox() {
         open("http://localhost:9999/");
         $("[data-test-id='city'] input").setValue("Москва");
-        $("[data-test-id='date'] input").setValue("29.12.2021");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(planningDate);
         $("[data-test-id='name'] input").setValue("Аркадий Паровозов");
         $("[data-test-id='phone'] input").setValue("+79112650042");
         $("[class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
         $("[data-test-id='agreement'] .checkbox__text").shouldHave(Condition.text("Я соглашаюсь с условиями обработки и использования моих персональных данных"));
 
     }
+    @Test
+    public void shouldCheckingTheFormMoreNumberPhone() {
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] input").setValue("Москва");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(planningDate);
+        $("[data-test-id='name'] input").setValue("Аркадий Паровозов");
+        $("[data-test-id='phone'] input").setValue("+791126500428");
+        $("[data-test-id='agreement']").click();
+        $("[class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
+        $("[data-test-id='phone'].input_invalid .input__sub").
+                shouldHave(Condition.text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+
+    }
+    @Test
+    public void shouldCheckingTheFormPhonePlus() {
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] input").setValue("Москва");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(planningDate);
+        $("[data-test-id='name'] input").setValue("Аркадий Паровозов");
+        $("[data-test-id='phone'] input").setValue("79112650042+");
+        $("[data-test-id='agreement']").click();
+        $("[class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
+        $("[data-test-id='phone'].input_invalid .input__sub").
+                shouldHave(Condition.text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+
+    }    @Test
+    public void shouldCheckingTheFormNoAdministrativeCenter() {
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] input").setValue("Бугульма");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(planningDate);
+        $("[data-test-id='name'] input").setValue("Аркадий Паровозов");
+        $("[data-test-id='phone'] input").setValue("+79112650042");
+        $("[data-test-id='agreement']").click();
+        $("[class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
+        $("[data-test-id='city'].input_invalid .input__sub").
+                shouldHave(Condition.text("Доставка в выбранный город недоступна"));
+
+    }
+
 
 }
 
